57fe389e8903f469e8d7faa8aa797d92f1cce31d
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-06-03 14:06:40 -0500

    add files

---
+++ form/thankyou.inc.php
@@ -0,0 +1,198 @@
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	border: 0;
+}
+
+article, aside, details, figcaption, figure, footer, header, main, nav, section, summary {
+    display:block;
+}
+
+body {
+	background-color: #f5f6f6;	
+}
+
+a:link, a:visited {
+ 	color: #eb2327;
+ 	text-decoration: none;
+ }
+
+h1 {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: 2rem;
+	text-align: left;
+	margin: 20px;
+	color: #101138;
+}
+
+fieldset {
+	color: #eb2327;
+	border: double 1px;
+	width: 90%;
+	margin: 10px;
+	margin-bottom: 20px;
+}
+
+.label {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: 1rem;
+
+}
+
+#line {
+	color: #eb2327;
+	border: double 2px;
+	width: 40px;
+	margin: 20px;
+	margin-top: -8px;
+}
+
+#discussion { 
+	margin-top: 20px;
+	text-align: justify;
+	padding: 10px;
+}
+
+legend {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: 1.2rem;
+	text-align: center;
+}
+
+
+p {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: .7rem;
+	margin-left: 25px;
+}
+
+aside {
+	text-align: left;
+	margin: 10px;
+	line-height: 1rem;
+}
+
+input[type="text"], input[type="email"], input[type="tel"], textarea {
+	border: .5px solid #D3D3D3;
+	background-color: #f5f6f6;
+	border-top: none;
+	border-right: none;
+	border-left-style: none;
+	margin-top: 5px;
+}
+
+input[placeholder="Your Name"], input[placeholder="Your Email"], input[placeholder="Your Phone Number"] {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: .7rem;
+	margin: 10px;
+	padding: 10px;
+	text-align: center;
+}
+
+.inline {
+	font-family: 'roboto condensed', sans-serif;
+	font-size: .7rem;
+	margin: 10px;
+}
+
+#preference {
+	font-size: .9rem;
+	margin: 10px;
+	margin-left: 20px;
+}
+
+#yes {
+	margin-left: 20px;
+	margin-bottom: 20px;
+}
+
+#homepage {
+	margin: 10px;
+	padding: 5px;
+	font-family: 'roboto condensed', sans-serif;
+	text-align: center;
+	width: 90%;
+	background-color: #ffffff;
+	color: #000000;
+	border: double .5px;
+	margin-bottom: 30px;
+}
+
+.center {
+	margin: 10px;
+	margin-top: 20px;
+	font-family: 'roboto condensed', sans-serif;
+	font-size: 1rem;
+}
+
+#contactmethod {
+	font-family: 'roboto condensed', sans-serif;
+	text-align: center;
+	font-size: .9rem;
+	margin: 10px;
+	margin-bottom: 20px;
+
+}
+
+#why {
+	margin: 10px;
+	padding: 5px;
+	font-family: 'roboto condensed', sans-serif;
+	text-align: center;
+	width: 90%;
+	background-color: #ffffff;
+	color: #000000;
+	border: double .5px;
+	margin-bottom: 30px;
+}
+
+#comments {
+	margin: 10px;
+	padding: 5px;
+	font-family: 'roboto condensed', sans-serif;
+	text-align: center;
+	width: 90%;
+	background-color: #ffffff;
+	color: #000000;
+	border: double .5px;
+	margin-bottom: 30px;
+	height: 30vh;
+}
+
+input[type="submit"] {
+	margin-right: 20px;
+}
+
+input[type="submit"], input[type="reset"] {
+	background-color: #101138;
+	color: #ffffff;
+	font-size: 1rem;
+	text-transform: uppercase;
+	padding: 10px;
+	margin: 10px;
+	margin-bottom: 30px;
+}
+
+input[type="submit"]:hover, input[type="reset"]:hover {
+	background-color: #eb2327;
+	opacity: .5;
+}
+
+.formrequiredincomplete {
+		background: #f5f6f6;
+		color: #000000;
+		border: #000000 1px solid;
+}
+
+</style>
+
+		<article id="contact_form">
+
+			<h1>Thank you!</h1>
+
+			<fieldset id="line"></fieldset>
+
+			<p>Your form has been submitted and we will be in touch shortly. Click here to go back to <a href="contact.html">We Are Generation Why</a>.</p>
+
+		</article>
